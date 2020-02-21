6fbbe916af9a80c130522015d685ccd1957bb006
Yash Bagri <ybagri@gmail.com>
2020-02-21 01:36:52 -0500

    trying to get css working on heroku

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,11 +1,11 @@
-const fs = require('fs');  // pull in the file system module
+const fs = require('fs'); // pull in the file system module
 
 const index = fs.readFileSync(`${__dirname}/../hosted/client.html`);
 const css = fs.readFileSync(`${__dirname}/../hosted/style.css`);
 const bundle = fs.readFileSync(`${__dirname}/../hosted/bundle.js`);
 
 const getIndex = (request, response) => {
-  response.writeHead(200, { 'Content-Tyspe': 'text/html' });
+  response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(index);
   response.end();
 };
